60990612cf50ea1000b0ef7e1f1d7d36fd3afb68
mostafa <mustafa.bahr12323@gmail.com>
2023-07-08 00:40:54 +0300

    7 / 8

--- lib/screens/student_home/home.dart
+++ lib/screens/student_home/home.dart
@@ -432,7 +432,7 @@ class _HomeState extends State<Home> {
               //     }
               //
               // ),
-
+//
               ConditionalBuilder(
                 condition: state is  GetAllVidoesSuccessState,
                 fallback: (context)=>Center(
